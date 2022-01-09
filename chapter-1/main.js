737b39a6075e8d583dd9fac01f632b74492c56b0
Juhye-Kim <kjh960215@gmail.com>
2022-01-09 19:34:30 +0900

    블록 검증 추가

--- chapter-1/main.js
+++ chapter-1/main.js
@@ -1,7 +1,9 @@
 import merkle from "merkle";
-import { Block, BlockHeader } from "./Block";
+import fs from "fs";
+import { Block, BlockHeader } from "./Block.js";
+import calculateHashForBlock from "./calculateHash.js";
 
-var blockchain = [getGenesisBlock()];
+let blockchain = [getGenesisBlock()];
 
 /**
  * get blockchain
@@ -43,4 +45,137 @@ function getGenesisBlock() {
     merkleRoot
   );
   return new Block(header, data);
+}
+
+/**
+ * get current version of blockchain
+ * @returns {string} current version
+ */
+function getCurrentVersion() {
+  const packageJson = fs.readFileSync("./package.json");
+  const currentVersion = JSON.parse(packageJson).version;
+
+  return currentVersion;
+}
+
+/**
+ * get current timestamp
+ * @returns {number} current timestamp
+ */
+function getCurrentTimeStamp() {
+  return Math.round(new Date().getTime() / 1000);
+}
+
+/**
+ * generate new block
+ * @param {any} blockData
+ * @returns {BLock} block
+ */
+function generateNextBlock(blockData) {
+  const previousBlock = getLatestBlock();
+  const currentVersion = getCurrentVersion();
+  const nextIndex = previousBlock.header.index + 1;
+  const previousHash = calculateHashForBlock(previousBlock);
+  const nextTimestamp = getCurrentTimeStamp();
+
+  const merkleTree = merkle("sha256").sync(blockData);
+  const merkleRoot = merkleTree.root() || "0".repeat(64);
+
+  const newBlockHeader = new BlockHeader(
+    currentVersion,
+    nextIndex,
+    previousHash,
+    nextTimestamp,
+    merkleRoot
+  );
+  return new Block(newBlockHeader, blockData);
+}
+
+/**
+ * @param {Block} newBlock
+ * @param {Block} previousBlock
+ * @returns {boolean} isValid
+ */
+function isValidNewBlock(newBlock, previousBlock) {
+  if (!isValidBlockStructure(newBlock)) {
+    console.log("Invalid block structure: %s", JSON.stringify(newBlock));
+    return false;
+  }
+
+  if (previousBlock.header.index + 1 !== newBlock.header.index) {
+    console.log("Invalid index");
+    return false;
+  }
+
+  if (calculateHashForBlock(previousBlock) !== newBlock.header.previousHash) {
+    console.log("Invalid previousHash");
+    return false;
+  }
+
+  if (
+    (newBlock.data.length !== 0 &&
+      merkle("sha256").sync(newBlock.data).root() !==
+        newBlock.header.merkleRoot) ||
+    (newBlock.data.length === 0 &&
+      "0".repeat(64) !== newBlock.header.merkleRoot)
+  ) {
+    console.log("Invalid merkleRoot");
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * @param {Block} block
+ * @returns {boolean} isValid
+ */
+function isValidBlockStructure(block) {
+  const { version, index, previousHash, timestamp, merkleRoot } = block.header;
+  const { data } = block;
+
+  return (
+    typeof version === "string" &&
+    typeof index === "number" &&
+    typeof previousHash === "string" &&
+    typeof timestamp === "number" &&
+    typeof merkleRoot === "string" &&
+    typeof data === "object"
+  );
+}
+
+/**
+ * @param {Block[]} blockchain
+ * @returns {boolean} isValid
+ */
+function isValidChain(blockchainToValidate) {
+  if (
+    JSON.stringify(blockchainToValidate[0] !== JSON.stringify(getGenesisBlock))
+  ) {
+    return false;
+  }
+
+  let tempBlocks = [blockchainToValidate[0]];
+
+  for (let i = 1; i < blockchainToValidate.length; i++) {
+    if (isValidBlockStructure(blockchainToValidate[i], tempBlocks[i - 1])) {
+      tempBlocks.push(blockchainToValidate[i]);
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ *
+ * @param {Block} newBlock
+ * @returns {boolean} isAdded
+ */
+function addBlock(newBlock) {
+  if (isValidNewBlock(newBlock, getLatestBlock())) {
+    blockchain.push(newBlock);
+    return true;
+  }
+  return false;
 }
