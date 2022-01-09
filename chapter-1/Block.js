545521518b993ad29be33a48b6b706ce3c7b0000
Juhye-Kim <kjh960215@gmail.com>
2022-01-09 19:04:34 +0900

    Block 클래스 주석 추가

--- chapter-1/Block.js
+++ chapter-1/Block.js
@@ -1,11 +1,22 @@
-export default class Block {
+export class Block {
+  /**
+   * @param {BlockHeader} header
+   * @param {any} data
+   */
   constructor(header, data) {
     this.header = header;
     this.data = data;
   }
 }
 
-class BlockHeader {
+export class BlockHeader {
+  /**
+   * @param {string} version
+   * @param {number} index
+   * @param {string} previousHash
+   * @param {number} timestamp
+   * @param {string} merkleRoot
+   */
   constructor(version, index, previousHash, timestamp, merkleRoot) {
     this.version = version;
     this.index = index;
